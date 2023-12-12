20160c2fd60e5eb4ac99e3500be2bdfb98fd9491
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-12-12 14:32:11 +0530

    python examples

---
+++ 01_Basic Python/02 Data Types/02 Tuple/Tuple exercises with solution/02_with_diff_data_types.py
@@ -0,0 +1,5 @@
+"""
+2. Write a Python program to create a tuple with different data types.
+"""
+tuple1 = ("tuple", False, 3.2, True, 1, [1, 'tejas', 2.5, ])
+print(tuple1)
---
+++ 01_Basic Python/02 Data Types/02 Tuple/Tuple exercises with solution/04_unpacking.py
@@ -0,0 +1,3 @@
+"""
+4. Write a Python program to unpack a tuple into several variables.
+"""
