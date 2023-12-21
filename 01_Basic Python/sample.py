c753acf0a889a18e3b87a256152ae8a69555ab6e
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-12-21 12:02:22 +0530

    add exp of a type of inheritance

--- 01_Basic Python/sample.py
+++ 01_Basic Python/sample.py
@@ -1,37 +1,8 @@
-"""
-n1 = int(input())
-n2 = int(input())
-
-sum = n1 + n2 # add()
-# even or odd
-if sum%2 == 0: # check()
-    print("even number")
-else:
-    print("Odd number")
-
-write a python function
-1) Ask 2 number from user
-2) Calculate total of 2 number
-3) Print if the sum of addition is even or odd
-
-
-Create 2 function 1)add(), 2) check
-Add() for addition
-Check() for check number is even or odd
-"""
-
-def add(n1, n2):
-    sum = n1 + n2
-    print(sum)
-    return sum
-
-def check(sum):
-    if sum%2 == 0:
-        print("even")
-    else:
-        print("Odd")
-
-n1 = int(input("N1 : "))
-n2 = int(input("N2 : "))
-x = add(n1, n2)
-check(x)
+def addition(num1, num2):
+    total = num1 + num2
+    # print(total)
+    return total
+    print("-------------")
+    
+x = addition(100, 200)
+print(x)
---
+++ 02_Intermediate Python/01 OOP/Inheritance/single.py
@@ -0,0 +1,13 @@
+class P:
+    def m1(self):
+        print("This is parent m1 method")
+
+
+class c(P):
+    def m2(self):
+        print("This is child m2 method")
+
+
+jay = c()
+jay.m2()
+jay.m1()
