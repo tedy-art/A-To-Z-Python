9762ef6799a7815fb735b2ff13a84addaa0a0476
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-12-25 10:53:26 +0530

    lambda exp

--- 02_Intermediate Python/03 Higher order function/03_storing_functions_data_structures.py
+++ 02_Intermediate Python/03 Higher order function/03_storing_functions_data_structures.py
@@ -9,6 +9,6 @@ def cube(x):
 operations = [square, cube]
 
 result_square = operations[0](3)  # 0 meaning index 0(square) and x = 3
-result_cube = operations[1](3)    # 1 meaning index 1(cube) and x = 3
+result_cube = operations[1](3)  # 1 meaning index 1(cube) and x = 3
 print(result_square)
 print(result_cube)
