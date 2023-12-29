31069a7349332c3e69688d2bae8beb8de7943181
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-12-29 19:41:40 +0530

    Arrays

---
+++ 04_Data Structures/01_array/02_access_element.py
@@ -0,0 +1,11 @@
+my_array = [10, 20, 30, 40, 50]
+print(f"array : {my_array}")
+
+# accessing the element from an array
+first_element = my_array[0]
+print(f"First element in array is : {first_element}")
+
+
+# Access 3rd element
+third_element = my_array[2]
+print(f"Third element in array is : {third_element}")
