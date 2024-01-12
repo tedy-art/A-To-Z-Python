99d909d8200f9c2ef5de62f3424a370d044174a9
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-01-12 13:31:15 +0530

    Array - practice set

---
+++ 01_Basic Python/02 Data Types/01 list/Practise Set/01_sum_of_list_ele.py
@@ -0,0 +1,7 @@
+list1 = [55, 66, 8, 2, 4, 54, 41]
+result = 0
+
+for ele in range(len(list1)):
+    result += list1[ele]
+
+print(f"result : {result}")
---
+++ 01_Basic Python/02 Data Types/01 list/Practise Set/02_List_reversel.py
@@ -0,0 +1,4 @@
+"""
+List Reversal:
+Create a program to reverse the elements of a list in-place.
+"""
