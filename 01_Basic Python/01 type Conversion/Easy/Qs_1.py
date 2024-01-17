a2911a9d509773441c28d22e539448ff8fed28a9
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-01-17 10:50:47 +0530

    TYPES CONVERSION - practice set

---
+++ 01_Basic Python/01 type Conversion/Easy/Qs_1.py
@@ -0,0 +1,8 @@
+"""
+Question 1:
+Convert the string "123" to an integer.
+"""
+str_in = "123"
+print(f"string : {str_in}, type : {type(str_in)}")
+int_out = int(str_in)
+print(f"Integer : {int_out}, type : {type(int_out)}")
---
+++ 01_Basic Python/01 type Conversion/Easy/Qs_4.py
@@ -0,0 +1,8 @@
+"""
+Question 4:
+Convert the string "5.6" to a float.
+"""
+str_in = "5.6"
+print(f"float : '{str_in}' and type : {type(str_in)}")
+float_out = float(str_in)
+print(f"float : {float_out} and type : {type(float_out)}")
