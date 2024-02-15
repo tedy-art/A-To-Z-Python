bc55799e80ee6a56f1f62baa8cea0db045a31d96
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-02-15 19:25:18 +0530

    alter

--- 02_Intermediate Python/02_Advanced Functions/Lambda/02_make_list_using_lambda.py
+++ 02_Intermediate Python/02_Advanced Functions/Lambda/02_make_list_using_lambda.py
@@ -2,7 +2,7 @@
 Take argument which will be number
 make a list 0 to N
 """
-make_list = lambda x:[i for i in range(0, x + 1)]
+make_list = lambda x: [i for i in range(0, x + 1)]
 list1 = make_list(9)
 list2 = make_list(5)
 
