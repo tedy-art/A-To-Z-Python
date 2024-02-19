eca158a9cbc40ac5fb2a6b7df781d0988c32efa1
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-02-19 09:50:56 +0530

    data type practise

--- 01_Basic Python/03 Data Types/exercise/easy/Qs6.py
+++ 01_Basic Python/03 Data Types/exercise/easy/Qs6.py
@@ -1,3 +1,16 @@
 """
 Question 6: Implement a program that extracts the unique elements from a list and returns a new list.
 """
+
+
+def element_count(random_list):
+    sorted_list = []
+    for ele in random_list:
+        if ele not in sorted_list:
+            sorted_list.append(ele)
+    return sorted_list
+
+
+random_list = [1, 2, 2, 3, 4, 4, 5]
+x = element_count(random_list)
+print(x)
