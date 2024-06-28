70c6ccc9aa1d5caeee6155ee240f763f0722b73b
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-06-28 19:31:44 +0530

    compitative coding

--- 09_Practise/square_list.py
+++
@@ -1,9 +0,0 @@
-"""
-Task
-The provided code stub reads and integer, n, from STDIN. 
-For all non-negative integers `i<n`, print `i2`.
-"""
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(n):
-        print(i*i)
