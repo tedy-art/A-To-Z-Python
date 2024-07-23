34526c056f48d99f5214d65e0bb801c9f1a8c90e
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-07-23 20:23:50 +0530

    Python Modules - Custom modules - module

--- 02_Intermediate Python/06 Modules/module_invoking.py
+++ 02_Intermediate Python/06 Modules/module_invoking.py
@@ -1,5 +1,6 @@
 import mymodule
 import mymodule1
+from mymodule2 import person
 
 print(mymodule.greeting("Tejas"))
 a = mymodule1.person["name"]
@@ -7,3 +8,4 @@ b = mymodule1.person["age"]
 print(a)
 print(b)
 
+print(person["age"])
---
+++ 02_Intermediate Python/06 Modules/mymodule2.py
@@ -0,0 +1,11 @@
+# save file as "mymodule2.py"
+
+def greeting(name):
+    print(f"Hello, {name}")
+
+
+person = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
